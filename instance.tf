8e62ba91230750f4b0b73fe3edf61ff7da587feb
daniel <danielmayowataiwo@gmail.com>
2024-11-23 16:00:22 +0000

    chore: allow inbound traffic to mongodb and postgres

--- instance.tf
+++ instance.tf
@@ -42,6 +42,20 @@ resource "aws_security_group" "allow_ssh" {
     cidr_blocks = var.ALLOWED_IPS
   }
 
+  ingress {
+    from_port   = 27017
+    to_port     = 27017
+    protocol    = "tcp"
+    cidr_blocks = var.ALLOWED_IPS
+  }
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = var.ALLOWED_IPS
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
